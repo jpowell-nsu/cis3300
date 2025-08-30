378cd6ae38704020702ee0f30abb6803262d8aa5
jpowell-nsu <powellja@nsula.edu>
2025-08-30 17:00:54 -0500

    08/30/2025

---
+++ File IO Part 6 (CSV Input)/src/driver/Driver.java
@@ -0,0 +1,62 @@
+package driver;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+import com.opencsv.exceptions.CsvException;
+
+public class Driver {
+
+	public static void main(String[] args) throws IOException, CsvException {
+		String filename = "files/scores.csv";
+
+		readByLine(filename);
+		// readAllAndDisplay(filename);
+		// List<String[]> students = readAllAndReturn(filename);
+
+		/*
+		for (int i = 0; i < students.size(); i++) { String[] line = students.get(i);
+			double score = Double.parseDouble(line[4]);
+			System.out.printf("%09d %-15s %-15s %-25s %5.2f\n",
+				Integer.parseInt(line[0]), line[1], line[2], line[3], score);
+		}
+		*/
+	}
+
+	public static void readByLine(String filename) throws CsvValidationException, IOException {
+		FileReader filereader = new FileReader(filename);
+		CSVReader csvreader = new CSVReader(filereader);
+		String[] nextRecord;
+
+		csvreader.readNext();
+		while ((nextRecord = csvreader.readNext()) != null) {
+			System.out.println(Arrays.toString(nextRecord));
+		}
+		csvreader.close();
+	}
+
+	public static void readAllAndDisplay(String filename) throws IOException, CsvException {
+		FileReader filereader = new FileReader(filename);
+		CSVReader csvreader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+
+		List<String[]> alldata = csvreader.readAll();
+		csvreader.close();
+		for (String[] record : alldata) {
+			System.out.println(Arrays.toString(record));
+		}
+	}
+
+	public static List<String[]> readAllAndReturn(String filename) throws IOException, CsvException {
+		FileReader filereader = new FileReader(filename);
+		CSVReader csvreader = new CSVReaderBuilder(filereader).withSkipLines(1).build();
+		List<String[]> alldata = csvreader.readAll();
+		csvreader.close();
+
+		return alldata;
+	}
+}
---
+++ File IO Part 7 (CSV Input)/src/driver/Driver.java
@@ -0,0 +1,43 @@
+package driver;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+public class Driver {
+	
+	public static void main(String[] args) throws IOException, CsvException {
+		List<String[]> pokemonBlock = new LinkedList<>();
+		String filename = "files/pokemon_data.csv";		
+		FileReader filereader = new FileReader(filename);
+		CSVReader csvreader = new CSVReader(filereader);
+		String[] nextRecord;
+		int linecount = 0;
+		int blocksize = 5;
+			
+		csvreader.readNext(); // read the header row
+		while ((nextRecord = csvreader.readNext()) != null) {
+			pokemonBlock.add(nextRecord);
+			linecount++;
+			if (linecount % blocksize == 0) {
+				System.out.println("Processing... ");
+				for (String[] line : pokemonBlock) {
+					System.out.println(Arrays.toString(line));
+				}		
+				pokemonBlock.clear();
+			}
+		} 
+		if (!pokemonBlock.isEmpty()) {
+			System.out.println("Processing... ");
+			for (String[] line : pokemonBlock) {
+				System.out.println(Arrays.toString(line));
+			}
+		}
+		csvreader.close();
+	}
+}
